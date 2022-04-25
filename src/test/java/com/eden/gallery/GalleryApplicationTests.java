60ec2386670de4c3e8b30ddb0ea8edd14550c08b
nvh-gel <nvhien2703@outlook.com>
2022-04-24 22:34:58 -0700

    first commit

---
+++ src/test/java/com/eden/gallery/GalleryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eden.gallery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
